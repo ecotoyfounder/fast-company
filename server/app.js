873f9137d1a4edcd28b0903fc53ff5709366e696
Pavel <M3modelist@gmail.com>
2022-12-23 12:37:36 +0200

    add server

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+const cors = require('cors')
+const initDatabase = require('./startUp/initDatabase')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+
+app.use('/api', routes)
+
+
+const PORT = config.get('port') ?? 8080
+
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('Production')
+// } else {
+//   console.log('Development')
+// }
+
+async function start() {
+  try {
+    mongoose.connection.once('open', () => {
+      initDatabase()
+    })
+    await mongoose.connect(config.get('mongoUri'))
+    console.log(chalk.green(`MongoDB connected.`))
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}...`))
+    )
+  } catch (e) {
+    console.log(chalk.red(e.message))
+    process.exit(1)
+  }
+}
+
+start()
+
